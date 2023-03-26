4b9539ad277c6eb84723211ec4e7b5539e6080a3
Shailendra Nagori <shelunagori@gmail.com>
2023-03-26 06:58:54 +0000

    refactoring the code

--- dpk.js
+++ dpk.js
@@ -1,28 +1,20 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
+exports.getPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
+  let candidate = event?.partitionKey || crypto.createHash("sha3-512")
+    .update(JSON.stringify(event ?? ''))
+    .digest("hex");
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  } else if (!candidate) {
     candidate = TRIVIAL_PARTITION_KEY;
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
+  return candidate.length > MAX_PARTITION_KEY_LENGTH
+    ? crypto.createHash("sha3-512").update(candidate).digest("hex")
+    : candidate;
 };
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const {deterministicPartitionKey} = require("./dpk");
+const {getPartitionKey} = require("./dpk");
 
-console.log(deterministicPartitionKey());
+console.log(getPartitionKey());
